75b4eca047735c12ace2c72d2ebf559b55bcd3b9
HANG <wangxiao_h@126.com>
2022-03-15 21:27:14 +0800

    PC_Mybatis_plus

--- src/main/java/com/atguigu/mybatis_plus_demo/entity/User.java
+++ src/main/java/com/atguigu/mybatis_plus_demo/entity/User.java
@@ -1,8 +1,12 @@
 package com.atguigu.mybatis_plus_demo.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+
+import java.util.Date;
 
 /**
  * @Author: HANG
@@ -16,4 +20,9 @@ public class User {
     private String name;
     private Integer age;
     private String email;
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;  //create_time
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime; //update_time
 }
